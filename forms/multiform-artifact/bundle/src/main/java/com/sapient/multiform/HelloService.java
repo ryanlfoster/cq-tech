885e83e95926b30fcef341fc9c0c16f54ea5573d
vijayanandvk <vijayanandvk@gmail.com>
2013-09-23 18:59:31 +0530

    Eclipse commit test

--- forms/multiform-artifact/bundle/src/main/java/com/sapient/multiform/HelloService.java
+++ forms/multiform-artifact/bundle/src/main/java/com/sapient/multiform/HelloService.java
@@ -4,9 +4,5 @@ package com.sapient.multiform;
  * A simple service interface
  */
 public interface HelloService {
-    /**
-     * @return the name of the underlying JCR repository implementation
-     */
     public String getRepositoryName();
-
 }
