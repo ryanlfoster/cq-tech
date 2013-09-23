41762cf03996cbb2331079d2c2501138ebfc54cd
vijayanandvk <vijayanandvk@gmail.com>
2013-09-23 19:07:40 +0530

    asdf

--- forms/multiform-artifact/bundle/src/main/java/com/sapient/multiform/HelloService.java
+++ forms/multiform-artifact/bundle/src/main/java/com/sapient/multiform/HelloService.java
@@ -4,5 +4,10 @@ package com.sapient.multiform;
  * A simple service interface
  */
 public interface HelloService {
+    
+    /**
+     * @return the name of the underlying JCR repository implementations
+     */
     public String getRepositoryName();
+
 }
